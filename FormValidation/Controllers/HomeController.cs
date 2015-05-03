a3ec17656f878f215d14989c010580e7dd607e05
aetos <aetos@aerie.jp>
2015-05-03 16:09:47 +0900

    add action and form

--- FormValidation/Controllers/HomeController.cs
+++ FormValidation/Controllers/HomeController.cs
@@ -1,30 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace FormValidation.Controllers
+﻿namespace FormValidation.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using FormValidation.Models;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            return this.View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            return this.View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return this.View();
+        }
+
+        public ActionResult Post()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(
+            [Required] PostModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            return this.RedirectToAction("Post");
         }
     }
 }
---
+++ FormValidation/Models/PostModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FormValidation.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
+
+    public class PostModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
